443976878fe3d6840f3892657fe89ab063be0e2e
Jake Wray <jakewray@mailbox.org>
2026-02-10 10:35:01 -0800

    feat: Set up Nix flakes to manage the development environment and dependencies, and update .gitignore.

--- flake.nix
+++ flake.nix
@@ -10,9 +10,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, fenix, flake_utils }:
-    flake_utils.lib.eachDefaultSystem (system:
-      let
+  outputs = {
+    self,
+    nixpkgs,
+    fenix,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
         pkgs = import nixpkgs {
           inherit system;
         };
@@ -33,19 +38,19 @@
           pango
           atk
           gdk-pixbuf
-          libsoup
-          webkitgtk
+          libsoup_3
+          webkitgtk_4_1
           alsa-lib
           dbus
         ];
-
-      in
-      {
+      in {
         devShells.default = pkgs.mkShell {
-          buildInputs = [
-            toolchain
-            pkgs.pkg-config
-          ] ++ libs;
+          buildInputs =
+            [
+              toolchain
+              pkgs.pkg-config
+            ]
+            ++ libs;
 
           shellHook = ''
             export RUST_SRC_PATH="${toolchain}/lib/rustlib/src/rust/library"
